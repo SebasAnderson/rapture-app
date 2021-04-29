84cfaf0c3859e554adcd11abb760bc24ee5f6be6
SebasAnderson <Sebastian96anderson@gmail.com>
2021-04-29 20:26:03 -0300

    Arreglos

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,20 @@
 //creamos un redux store
-import { applyMiddleware, createStore, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
-//import data from "./data";
-import { productListReducer } from "./reducers/productReducers";
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
 
 const initialState = {};
-const reducer = combineReducers ({
-  productList : productListReducer,
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   initialState,
   composeEnhancer(applyMiddleware(thunk))
 );
-
 export default store;
