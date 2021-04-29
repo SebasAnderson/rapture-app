25386f19d66ac68e43c38c01a3a1a98d9df4c5bd
SebasAnderson <Sebastian96anderson@gmail.com>
2021-04-29 16:00:18 -0300

    instalamos react-redux, redux y redux-thuck. insex.js, store.js, productActions productreducers

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,21 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Product from '../components/Product';
+import React, { useEffect } from "react";
+//import axios from "axios";
+import Product from "../components/Product";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
-
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 //import data from "../data";
+
 //Testing
 //nodemon --exec npm run start
 export default function HomeScreen() {
-  const [products, setProducts] = useState([]); //react hook => manejar el estado de los comp
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    //Definimos fetch data
-    const fecthData = async () => {
-      try {
-        setLoading(true);                                                                                  
-        const { data } = await axios.get("/api/products");
-        //el arreglo del back se transfiera a "data" en el front
-        setLoading(false);
-        setProducts(data);
-      } catch (err) {
-        setError(err.mesagge);
-        setLoading(false);
-      }
-    };
-    fecthData();
-  }, []); //el arreglo va a lista de dependencias
+    dispatch(listProducts());
+  }, [dispatch]); //el arreglo va a lista de dependencias
   return (
     <div>
       {loading ? (
