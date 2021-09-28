fc1c99bdd27e0af4de5d63c0eb6fa287d7403398
SebasAnderson <Sebastian96anderson@gmail.com>
2021-09-28 03:01:42 -0300

    delete users up

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -95,5 +95,24 @@ userRouter.get(
     res.send(users);
   })
 );
+//API para borrar usuarios
+userRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      if (user.email === "admin@example.com") {
+        res.status(400).send({ message: "Can Not Delete Admin User" });
+        return;
+      }
+      const deleteUser = await user.remove();
+      res.send({ message: "User Deleted", user: deleteUser });
+    } else {
+      res.status(404).send({ message: "User Not Found" });
+    }
+  })
+);
 
 export default userRouter;
