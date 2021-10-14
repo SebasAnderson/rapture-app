a93ea033b3ae37a2ec398a2ad97a035924ab41da
SebasAnderson <Sebastian96anderson@gmail.com>
2021-10-14 05:58:12 -0300

    Rata & Review

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,8 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    comment: { type: String, required: true },
+    rating: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
-    seller: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
+    seller: { type: mongoose.Schema.Types.ObjectID, ref: "User" },
     image: { type: String, required: true },
     brand: { type: String, required: true },
     category: { type: String, required: true },
@@ -11,10 +23,11 @@ const productSchema = new mongoose.Schema(
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
+    reviews: [reviewSchema],
   },
   {
     timestamps: true,
   }
 );
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 export default Product;
